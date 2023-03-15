67df941b9e38e0345126728c13130d58061a6a10
femellone <38559436+femellone@users.noreply.github.com>
2023-03-15 11:30:03 -0300

    update march 2023

--- run_script.py
+++ run_script.py
@@ -2,12 +2,14 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("../../GOLSTATS/qatar-frontend/")
+ridr_app = git.Repo("../../../penguin/ridr-app/")
+ridr_server = git.Repo("../../../penguin/ridr-server/")
+ridr_admin = git.Repo("../../../penguin/ridr-admin/")
 # Your mock repo
-mock_repo = git.Repo("../../GOLSTATS/mock-repo/")
-importer = Importer([repo], mock_repo)
+mock_repo = git.Repo("../mock-repo/")
+importer = Importer([ridr_app, ridr_server, ridr_admin], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github profiles uses
 # my work email
-importer.set_author(['fernandomellone2@gmail.com', 'fernandomellone2@gmail.com'])
+importer.set_author(['fernandomellone2@gmail.com', '38559436+femellone@users.noreply.github.com'])
 importer.import_repository()
